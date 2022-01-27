f293fab8e604dab898bf6b7ac989a45ac003ea8a
nisarg001 <nisarg0606@gmail.com>
2022-01-27 14:25:13 +0530

    malloc Expalined

---
+++ Ayan/mallocexample.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int len; // varible
+    cout << "Enter the value of Len: ";
+    cin >> len;                             // 5
+    int *ptr;                               // pointer memory? -> 4 yes
+    ptr = (int *)malloc(sizeof(int) * len); // allocating memory to hold 5 integers
+    for (int i = 0; i < len; i++)
+    {
+        cout << "Enter a number: ";
+        cin >> *(ptr + i);
+    }
+    for (int i = 0; i < len; i++)
+    {
+        cout << "The value of ptr is: " << *(ptr + i) << endl;
+    }
+    free(ptr); // free the memory
+    return 0;
+}
